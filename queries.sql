b012ffc1d1f7297e6734d2fa2a7422d9a5a0a295
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-09-27 15:12:17 -0400

    Verified Gradle Build, set up + Tied in SQL database, inspected SQL database.  Task 1 complete

--- queries.sql
+++ queries.sql
@@ -1,5 +1,8 @@
 -- Part 1: Test it with SQL
-
+--COLUMN id TYPE int PK
+--COLUMN employer TYPE varchar(255)
+--COLUMN name TYPE varchar(255)
+--COLUMN skills TYPE varchar(255)
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
